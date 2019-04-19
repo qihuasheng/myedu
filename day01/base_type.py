8262411886dac17e6bbd65981f0bda0658c62eb9
unknown <2545907441@qq.com>
2019-04-19 10:32:11 +0800

    first commit

---
+++ day01/base_type.py
@@ -0,0 +1,56 @@
+
+def test1():
+    print('test1')
+def test2():
+    print('test2')
+def test3():
+    print('test3')
+def int_demo():
+    aint=6
+    print(aint)
+    print(type(aint))
+def str_demo():
+     astr='7'
+     print(astr)
+     print(type(astr))
+def float_demo():
+     float=7.5
+     print(float)
+     print(type(float))
+def add_demo(a,b):
+    print(a+b)
+def type_zhuanhuan():
+     aint=7
+     print(type(aint))
+     print(type( str(aint) ))
+def str_join():
+     a=5
+     b=5.8
+     c='什么'
+     print('%s %s %s'%(a,b,c))
+     print(str(a)+str(b)+c)
+def str_abc():
+    ab='这是什么'
+    b=55.5
+    cd='hhh'
+    print(ab+str(b)+cd)
+
+def jianfa_demo(a,b):
+     c=a-b
+     return c
+if __name__ == '__main__':
+    # test3()
+    # test1()
+    # test2()
+    # int_demo()
+    # str_demo()
+    # float_demo()
+    # add_demo(8,5)
+    # type_zhuanhuan()
+    # str_join()
+    # c=jianfa_demo(10,6)
+    # e=add_demo(1,7)
+    # print(e)
+    # print(c)
+    str_abc()
+
---
+++ day01/list_type.py
@@ -0,0 +1,7 @@
+
+from day01 import base_type
+
+if __name__ == '__main__':
+    value=base_type.jianfa_demo(7,8)
+    base_type.add_demo(11,5)
+    print(value)
