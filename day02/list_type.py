b5110c5512b466e1e3bbbaaeeecae2fba63b92b5
qihs2 <2545907441@qq.com>
2019-04-19 13:20:46 +0800

    frist commit

--- day02/list_type.py
+++ day02/list_type.py
@@ -1,8 +1,12 @@
 def list_sel(a):
     print(a[1])
     print(a[3])
+    print(a[1:5])
+    print(a[0:3])
+    print(a)
+    print(a[-3:-1])
 
 
 if __name__ == '__main__':
-    alist=[5,'shide',3,6,'啊']
+    alist=[5,'shide',3,6,'啊','5.8','ss','我的','123']
     list_sel(alist)
